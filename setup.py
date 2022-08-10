b6462f6c46df04b37e3de5192f24fff045f3addb
ykvch <yankevych@gmail.com>
2022-08-11 01:15:11 +0300

    Fix long_description

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 NAME = "compr"
 CLASSIFIERS = [
@@ -12,4 +12,5 @@ setup(
     version=__version__,
     package_dir={"": "src"},
     classifiers=CLASSIFIERS,
+    long_description_content_type='text/markdown',
 )
